f4abc8fd6925fb47bb9f251dbeb1135a9bb58a11
Eslim Daga <esliim9811@gmail.com>
2023-02-26 16:38:30 -0500

    feat:📝:Agregué el contenido inicial de la primera sección

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 export default function Home() {
 	return (
 		<>
-			<h1>Killamunay</h1>
+			<section className="w-full flex flex-col items-start justify-center gap-10 px-8">
+				<h1 className="font-bold text-6xl">Descrubre tu luna</h1>
+				<p className="font-light text-xl">
+					Descubre cómo lucía la luna en el momento en que tú o esa persona
+					especial nacieron.
+				</p>
+				<button className="bg-primary px-6 py-4 rounded-xl font-semibold text-lg text-dark-primary">
+					Comenzar
+				</button>
+			</section>
+			<section className="w-full"></section>
 		</>
 	);
 }
