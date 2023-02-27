72bed93a7e15c2ca824d089f8b71a77704b068bd
Eslim Daga <esliim9811@gmail.com>
2023-02-26 19:45:30 -0500

    feat:📝:Moví lo necesario del Header a un nuevo componente

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,49 +1,9 @@
-import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
-import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import Header from "./Header";
 
 export default function Layout({ children }) {
-	const [mounted, setMounted] = useState(false);
-	const { systemTheme, theme, setTheme } = useTheme();
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
-	const renderThemeChanger = () => {
-		if (!mounted) return null;
-
-		const currentTheme = theme === "system" ? systemTheme : theme;
-
-		if (currentTheme === "dark") {
-			return (
-				<SunIcon
-					className="w-10 h-10 z-10 text-primary "
-					role="button"
-					onClick={() => setTheme("light")}
-				/>
-			);
-		} else {
-			return (
-				<MoonIcon
-					className="w-10 h-10 z-10 text-primary "
-					role="button"
-					onClick={() => setTheme("dark")}
-				/>
-			);
-		}
-	};
-
 	return (
 		<>
-			<header className="w-full h-24 flex items-center justify-between px-8">
-				<img
-					src="/images/logo-killamunay-dark.png"
-					alt="logo-killamunay-dark"
-					className="z-10 lg:w-52 w-44"
-				/>
-				{renderThemeChanger()}
-			</header>
+			<Header />
 			<main className="flex justify-center section-hero">{children}</main>
 		</>
 	);
