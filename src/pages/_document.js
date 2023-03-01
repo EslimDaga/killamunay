ccb8ff7e4a5dbe0344f64c11a2e6305da3209636
Eslim Daga <esliim9811@gmail.com>
2023-02-28 22:14:12 -0500

    feat:📝:Eh quitado la etiqueta dell título y el viewport

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,8 @@ export default function Document() {
 	return (
 		<Html lang="es">
 			<Head>
-				<title>Killamunay - Descubre la luna</title>
 				<link rel="icon" href="/images/favicon.png" />
 				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta
 					name="description"
 					content="Killamunay es un nombre que fusiona el quechua y el español, que representa la unión del amor y la luna. Esta plataforma te permite descubrir cómo lucía la luna en el momento en que tú o esa persona especial nacieron. Con solo ingresar la fecha de nacimiento, podrás ver la imagen de la luna en su fase correspondiente. Con Killamunay, podrás sorprender a tus amigos, familiares y seres queridos con un detalle único y especial que refleja su conexión con el universo. ¡Descubre la luna de tus seres queridos con Killamunay!."
