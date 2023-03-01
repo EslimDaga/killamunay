5c74712118b5578d1253e68953d1af3ccb125a6f
Eslim Daga <esliim9811@gmail.com>
2023-02-28 22:13:46 -0500

    feat:📝:Eh agregado la etiqueta para el título y para el viewport

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 import { ThemeProvider } from "next-themes";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 import "../assets/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
 	return (
 		<ThemeProvider enableSystem={true} attribute="class">
+			<Head>
+				<title>Killamunay - Descubre la luna</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
